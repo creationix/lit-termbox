40a386e66d28ed17ea4de3489c3bb626f6a2588b
Tim Caswell <tim@creationix.com>
2015-04-14 22:29:17 -0500

    Use ffi-loader to wrok in luvit too

--- package.lua
+++ package.lua
@@ -1,7 +1,10 @@
 return {
   name = "creationix/termbox",
-  version = "1.0.4",
+  version = "1.0.5",
   homepage = "https://github.com/creationix/lit-termbox",
+  dependencies = {
+    "creationix/ffi-loader@1.0.0",
+  },
   files = {
     "*.lua",
     "*.h",
