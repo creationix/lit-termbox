5114849f5f1701d601281831b5457c532ce3879b
Tim Caswell <tim@creationix.com>
2015-07-17 15:57:51 -0500

    Update linux build to latest termbox

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ local names = {
 }
 
 local ffi = require('ffi')
-ffi.cdef(module:load("termbox.h"))
+ffi.cdef((module:load("termbox.h")))
 
 local arch = ffi.os .. "-" .. ffi.arch
 return module:action(arch .. "/" .. names[arch], function (path)
